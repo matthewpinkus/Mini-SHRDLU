de60554c8f7c8c225973a04fb95bcf7072da6529
Matthew <68981721+mattswell1@users.noreply.github.com>
2020-07-30 13:54:15 +1000

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,7 +2,6 @@
  * Data Structures and Algorithms
  * 15-04-2019
  * Assignment 1
- * ID:	1875 1084
  * Game.h
  *
  * This class receives user input board size, initialises the board with desired size, and calls
